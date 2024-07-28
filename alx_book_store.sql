447b9aaf98f6f7d513709ef59e25baf357585772
Seth-Gbemumu <gracesefor@gmail.com>
2024-07-28 10:12:11 -0700

    Create MYSQL database to store books

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 CREATE TABLE Books (
-book_id (Primary Key),
+book_id INT (Primary Key),
 title VARCHAR(130),
 author_id (Foreign Key referencing Authors table),
 price DOUBLE,
@@ -14,19 +14,19 @@ author_name VARCHAR(215)
 );
 
 CREATE TABLE Customers (
-customer_id (Primary Key),
+customer_id INT(Primary Key),
 customer_name VARCHAR(215),
 email VARCHAR(215),
 address TEXT
 );
 CREATE TABLE Orders (
-order_id (Primary Key),
+order_id INT (Primary Key),
 customer_id (Foreign Key referencing Customers table),
 order_date DATE
 );
 
 CREATE TABLE Order_Details (
-orderdetailid (Primary Key),
+orderdetail_id INT (Primary Key),
 order_id (Foreign Key referencing Orders table),
 book_id (Foreign Key referencing Books table),
 quantity DOUBLE
