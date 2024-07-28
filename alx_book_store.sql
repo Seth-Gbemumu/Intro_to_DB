4278cf53f9a6feb10aaedaed7f2aa0ac55e99037
Seth-Gbemumu <gracesefor@gmail.com>
2024-07-28 10:17:09 -0700

    Create MYSQL database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,33 +1,45 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-CREATE TABLE Books (
-book_id INT (Primary Key),
-title VARCHAR(130),
-author_id (Foreign Key referencing Authors table),
-price DOUBLE,
-publication_date DATE
-);
+USE alx_book_store;
 
 CREATE TABLE Authors (
-author_id (Primary Key),
-author_name VARCHAR(215)
+    author_id INT AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL,
+    PRIMARY KEY (author_id)
+);
+
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    PRIMARY KEY (book_id),
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE Customers (
-customer_id INT(Primary Key),
-customer_name VARCHAR(215),
-email VARCHAR(215),
-address TEXT
+    customer_id INT AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT,
+    PRIMARY KEY (customer_id)
 );
+
 CREATE TABLE Orders (
-order_id INT (Primary Key),
-customer_id (Foreign Key referencing Customers table),
-order_date DATE
+    order_id INT AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
-orderdetail_id INT (Primary Key),
-order_id (Foreign Key referencing Orders table),
-book_id (Foreign Key referencing Books table),
-quantity DOUBLE
+    orderdetailid INT AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    PRIMARY KEY (orderdetailid),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
